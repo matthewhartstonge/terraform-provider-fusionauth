a03f8c130ff0850b854abe5b1e174ac04253295d
Matthew Hartstonge <matt@mykro.co.nz>
2023-07-11 09:10:19 +1200

    feat(tools): adds support for document generation.

---
+++ tools/tools.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Matthew Hartstonge <matt@mykro.co.nz>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package tools
+
+import (
+	// Documentation generation
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
